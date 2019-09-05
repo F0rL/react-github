087cb7a09ef8e178209b3eb065b9709095fd5ab9
Forl <kumaloveslife@gmail.com>
2019-09-05 16:35:27 +0800

    配置_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,31 +6,29 @@
  * 3.使用componentDidCatch自定义处理错误
  * 4.注入额外数据到页面里 (如 GraphQL 查询)
  */
-import App,{Container} from 'next/app';
-import React from 'react';
-import Layout from '../components/Layout';
+import App, { Container } from "next/app";
+import React from "react";
+import Layout from "../components/Layout";
 
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
-class MyApp extends App{
-    //每次页面切换都执行此方法
-    static async getInitialProps({Component,router,ctx}){
-        let pageProps={};
-        if(Component.getInitialProps){
-            pageProps = await Component.getInitialProps(ctx);
-        }
-        return {pageProps};
+class MyApp extends App {
+  //每次页面切换都执行此方法
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
+    return { pageProps };
+  }
 
-    render(){
-        const { Component,pageProps } = this.props;
-        return (
-            <Container>
-                <Layout>
-                  <Component {...pageProps}/>
-                </Layout>
-            </Container>
-        )
-    }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
 }
 export default MyApp;
