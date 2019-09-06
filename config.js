f83e9ae610eeeced345d8b32b3e8918da9555ae6
Forl <kumaloveslife@gmail.com>
2019-09-07 01:25:03 +0800

    session初步设置

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports ={
+  github: {
+    client_id: 'Iv1.4242a665309979d7',
+    client_secret: '934f71e2ca6469c324eb14651372c0498e45339e'
+  }
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const next = require("next");
+const session = require("koa-session")
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -10,6 +11,31 @@ app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
 
+  server.keys = ['kuma develop github']
+  const SESSION_CONFIG = {
+    key: 'gid'
+  }
+  server.use(session(SESSION_CONFIG,server))
+
+  server.use(async (ctx, next) => {
+    if(ctx.cookies.get('gid')){
+      ctx.session = {}
+    }
+    await next()
+  })
+  // server.use(async (ctx, next) => {
+  //   // console.log(ctx.cookies.get('id'))
+  //   //获取用户数据
+  //   if(ctx.session.user) {
+  //     ctx.session.user = {
+  //       name: 'kuma',
+  //       age: 22
+  //     }
+  //   }else {
+  //     console.log('session is',  ctx.session.user)
+  //   }
+  //   await next()
+  // })
   router.get("/a/:id", async (ctx) => {
     const id = ctx.params.id;
     await handle(ctx.req, ctx.res, {
@@ -19,10 +45,21 @@ app.prepare().then(() => {
     ctx.respond = false;
   });
 
+  router.get("/set/user", async (ctx) => {
+    ctx.session.user ={
+      name: 'kuma',
+      age: 22
+    }
+    ctx.body = 'set session success'
+  });
+
   server.use(router.routes());
 
   server.use(async (ctx, next) => {
+    ctx.cookies.set('id', 'userID:xxxxxxxxxxxxxx')
     await handle(ctx.req, ctx.res);
+
+    //不在使用koa内置的对body处理
     ctx.respond = false;
   });
 
